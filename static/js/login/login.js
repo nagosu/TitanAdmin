f872600d845d100e2f29948b391ed4dcdff27ebb
nagosu <nozick1021@gmail.com>
2024-09-17 04:34:54 +0900

    [2-1] 로그인 퍼블리싱

---
+++ static/js/login/login.js
@@ -0,0 +1,28 @@
+const loginButton = document.getElementById("loginButton");
+const loginModalCloseButton = document.getElementById("loginModalCloseButton");
+const loginModalConfirm = document.getElementById("loginModalConfirm");
+const loginModalMessage = document.getElementById("loginModalMessage");
+
+function openLoginErrorModal() {
+  loginModalConfirm.style.display = "flex";
+}
+
+function closeLoginErrorModal() {
+  loginModalConfirm.style.display = "none";
+}
+
+function setLoginErrorModalMessage() {
+  loginModalMessage.innerHTML =
+    "아이디 or 패스워드가 맞지 않습니다.<br />다시 시도해주세요.";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loginButton.addEventListener("click", () => {
+    setLoginErrorModalMessage();
+    openLoginErrorModal();
+  });
+
+  loginModalCloseButton.addEventListener("click", () => {
+    closeLoginErrorModal();
+  });
+});
